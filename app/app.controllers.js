e4e067b145f597f6cd0c9fb29ac3b7f626623868
Matt Geale <matthewgeale@me.com>
2017-03-27 13:57:04 +1100

    fixing first commit

---
+++ app/app.controllers.js
@@ -0,0 +1,37 @@
+app.controller('mainController', function($scope, $http, $location) {
+
+  $http.get('data/warphotographs.data.json')
+  .then(function(response) {
+    $scope.imgs = response.data;
+  });
+
+  var lightboxImg;
+  lightboxImg = document.getElementById('lightbox-img');
+
+  $scope.lClass = 'close';
+
+  var toggleClass = function() {
+    if ($scope.lClass == 'open') {
+      $scope.lClass = 'close';
+    } else {
+      $scope.lClass = 'open';
+    }
+  }
+
+  $scope.photoClick = function() {
+    $scope.lightbox = {
+      img: this.img.highreslink,
+      caption: this.img.caption
+    }
+    toggleClass();
+    var delay = setTimeout(function() {
+      echo.init();
+    }, 0);
+  }
+
+  $scope.toggleClick = function() {
+    toggleClass();
+    lightboxImg.src = 'images/empty.svg';
+  }
+
+});
